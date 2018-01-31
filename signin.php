4ed559872fabfee29bf10cc1457cdec1758cf91f
ddany <duminydaniel@gmail.com>
2018-02-01 03:02:13 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,7 +1,7 @@
-<?php
+<?php  //Start the Session
+session_start();
 include 'connect.php';
 include 'header.php';
- 
 //3. If the form is submitted or not.
 //3.1 If the form is submitted
 if (isset($_POST['username']) and isset($_POST['password'])){
@@ -30,6 +30,42 @@ echo "This is the Members Area
 ";
 echo "<a href='logout.php'>Logout</a>";
  
-}
-include 'footer.php';
+}else{
+//3.2 When the user visits the page first time, simple login form will be displayed.
 ?>
+<html>
+<head>
+	<title>User Login Using PHP & MySQL</title>
+	
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+<link rel="stylesheet" href="styles.css" >
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+      <form class="form-signin" method="POST">
+      <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+        <h2 class="form-signin-heading">Please Login</h2>
+        <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
+	  <input type="text" name="username" class="form-control" placeholder="Username" required>
+	</div>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+        <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>
+      </form>
+</div>
+
+</body>
+
+</html>
+<?php } ?>
