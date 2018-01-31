1cd27b06a7ec09556a686990a7c06d5b1dc6c944
ddany <duminydaniel@gmail.com>
2018-02-01 03:26:26 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,62 +1,82 @@
 <?php
-        include 'header.php';
- 	include 'connect.php';
-    // If the values are posted, insert them into the database.
-    if (isset($_POST['username']) && isset($_POST['password'])){
-        $username = $_POST['username'];
-	$email = $_POST['email'];
-        $password = $_POST['password'];
+include 'connect.php';
+include 'header.php';
  
-        $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
-        $result = mysqli_query($connection, $query);
-        if($result){
-            $smsg = "User Created Successfully.";
-        }else{
-            $fmsg ="User Registration Failed";
+echo '<h3>Sign up</h3>';
+ 
+if($_SERVER['REQUEST_METHOD'] != 'POST')
+{
+    echo '<form method="post" action="">
+        Username: <input type="text" name="user_name" /><br>
+        Password: <input type="password" name="user_pass"><br>
+        Password again: <input type="password" name="user_pass_check"><br>
+        E-mail: <input type="email" name="user_email">
+        <input type="submit" value="Register" />
+     </form>';
+}
+else
+{
+    $errors = array();
+    if(isset($_POST['user_name']))
+    {
+        if(!ctype_alnum($_POST['user_name']))
+        {
+            $errors[] = 'The username can only contain letters and digits.';
+        }
+        if(strlen($_POST['user_name']) > 30)
+        {
+            $errors[] = 'The username cannot be longer than 30 characters.';
         }
     }
-    ?>
-<html>
-<head>
-	<title>User Registeration Using PHP & MySQL</title>
-	
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
-
-<link rel="stylesheet" href="styles.css" >
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-      <form class="form-signin" method="POST">
-      
-      <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
-      <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
-        <h2 class="form-signin-heading">Please Register</h2>
-        <div class="input-group">
-	  <span class="input-group-addon" id="basic-addon1">@</span>
-	  <input type="text" name="username" class="form-control" placeholder="Username" required>
-	</div>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
-      </form>
-</div>
-
-</body>
-
-</html>
+    else
+    {
+        $errors[] = 'The username field must not be empty.';
+    }
+     
+     
+    if(isset($_POST['user_pass']))
+    {
+        if($_POST['user_pass'] != $_POST['user_pass_check'])
+        {
+            $errors[] = 'The two passwords did not match.';
+        }
+    }
+    else
+    {
+        $errors[] = 'The password field cannot be empty.';
+    }
+     
+    if(!empty($errors)) 
+    {
+        echo 'Uh-oh.. a couple of fields are not filled in correctly..';
+        echo '<ul>';
+        foreach($errors as $key => $value)
+        {
+            echo '<li>' . $value . '</li>';
+        }
+        echo '</ul>';
+    }
+    else
+    {
+        $sql = "INSERT INTO
+                    users(user_name, user_pass, user_email ,user_date, user_level)
+                VALUES('" . mysql_real_escape_string($_POST['user_name']) . "',
+                       '" . sha1($_POST['user_pass']) . "',
+                       '" . mysql_real_escape_string($_POST['user_email']) . "',
+                        NOW(),
+                        0)";
+                         
+        $result = mysql_query($sql);
+        if(!$result)
+        {
+            echo 'Something went wrong while registering. Please try again later.';
+        }
+        else
+        {
+            echo 'Successfully registered. You can now <a href="signin.php">sign in</a> and start posting! :-)';
+        }
+    }
+}
+ 
+include 'footer.php';
+?>
