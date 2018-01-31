9ff216e345e118ae80aa4140b60ddabaad9fefd7
ddany <duminydaniel@gmail.com>
2018-02-01 02:54:36 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,9 +2,6 @@
 include 'connect.php';
 include 'header.php';
  
-<?php  //Start the Session
-session_start();
- require('connect.php');
 //3. If the form is submitted or not.
 //3.1 If the form is submitted
 if (isset($_POST['username']) and isset($_POST['password'])){
